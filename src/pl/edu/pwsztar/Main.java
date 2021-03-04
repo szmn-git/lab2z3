d829ae92860b80d59464bbad5b22e900f1dc084c
a <@gmail.com>
2021-03-04 11:04:27 +0100

    feature-e sum

--- src/pl/edu/pwsztar/Main.java
+++ src/pl/edu/pwsztar/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println(sum(4));
+    }
+
+
+    static private int sum(int x) {
+        int ans = 0;
+        for(int i =0; i <= x; i++) {
+            ans += i;
+        }
+
+        return ans;
     }
 }
